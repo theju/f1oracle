aadb99fa347090a24ad2c66b297186594954b275
Thejaswi Puthraya <thejaswi.puthraya@gmail.com>
2013-03-09 12:15:13 +0530

    Added the race app to the list of INSTALLED_APPS.

--- f1oracle/settings.py
+++ f1oracle/settings.py
@@ -123,6 +123,8 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.admin',
+
+    'race',
 )
 
 # A sample logging configuration. The only tangible logging
