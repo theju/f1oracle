be27ecbcb8693a08106a6496c0475a7250770a4a
Thejaswi Puthraya <thejaswi.puthraya@gmail.com>
2013-03-10 18:32:26 +0530

    Initial iteration of the dashboard page.

--- race/urls.py
+++ race/urls.py
@@ -2,4 +2,8 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     url('^dashboard/$', 'race.views.dashboard', name='dashboard'),
+    url('^dashboard/overall_race/driver/$', 'race.views.overall_driver_prediction'),
+    url('^dashboard/overall_race/constructor/$', 'race.views.overall_constructor_prediction'),
+    url('^dashboard/race(?P<race_id>\d+)/driver/$', 'race.views.race_driver_prediction'),
+    url('^dashboard/race(?P<race_id>\d+)/constructor/$', 'race.views.race_constructor_prediction'),
 )
--- race/views.py
+++ race/views.py
@@ -1,2 +1,25 @@
+from .models import Driver, Constructor, Race
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+
 def dashboard(request):
+    drivers = Driver.objects.all()
+    constructors = Constructor.objects.all()
+    races = Race.objects.all()
+    return render_to_response("race/dashboard.html",
+                              {"races": races,
+                               "drivers": drivers,
+                               "constructors": constructors},
+                              context_instance=RequestContext(request))
+
+def overall_driver_prediction(request):
+    pass
+
+def overall_constructor_prediction(request):
+    pass
+
+def race_driver_prediction(request, race_id=None):
+    pass
+
+def race_constructor_prediction(request, race_id=None):
     pass
