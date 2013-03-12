96804b8a8664c9a92ef0b8e3a16e3ff0847c7fd0
Thejaswi Puthraya <thejaswi.puthraya@gmail.com>
2013-03-12 21:55:21 +0530

    Added two more models to the admin.

--- race/admin.py
+++ race/admin.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from .models import Country, Race, Constructor, Driver, Result, \
     RaceDriverPrediction, RaceConstructorPrediction, OverallDriverPrediction, \
     OverallConstructorPrediction, OverallDriverPredictionHistory, \
-    OverallConstructorPredictionHistory, RaceUserWinner
+    OverallConstructorPredictionHistory, RaceUserWinner, Result
 
 admin.site.register(Country)
 admin.site.register(Race)
 admin.site.register(Constructor)
 admin.site.register(Driver)
+admin.site.register(Result)
+admin.site.register(RaceUserWinner)
